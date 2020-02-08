aa68f4a285b6871d285dda7f687e9c5abc3aab20
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-02-08 00:04:59 -0400

    Add GET and POST for '/api/resources'

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const server = express();
 
 const ProjectsRouter = require("./routes/projectsRouter.js");
+const ResourcesRouter = require("./routes/resourcesRouter.js");
 
 server.use(express.json());
 server.use("/api/projects", ProjectsRouter);
+server.use("/api/resources", ResourcesRouter);
 
 module.exports = server;
